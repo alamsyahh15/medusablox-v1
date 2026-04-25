6a7c897b0092c5cafe2a7b810caadf49ad34ee9b
alamsyahh15 <black.taurus001@gmail.com>
2026-04-25 16:38:47 +0700

    update time

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -170,7 +170,7 @@ export default function OrderForm() {
     return nowMinute >= minute;
   };
   const promoDiscount = (() => {
-    if (!isAfterOrAtWibTime(16, 0)) return 0;
+    if (!isAfterOrAtWibTime(19, 45)) return 0;
     if (method === 'gamepass' && activeRobux >= 2000) return 20000;
     if (method !== 'group') return 0;
     if (activeRobux === 2000) return 20000;
