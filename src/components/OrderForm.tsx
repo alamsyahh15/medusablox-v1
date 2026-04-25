e5cd887c14cef565d31bae05843f8c8e0aaee7f0
alamsyahh15 <black.taurus001@gmail.com>
2026-04-25 16:41:20 +0700

    fix promo

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -170,12 +170,12 @@ export default function OrderForm() {
     return nowMinute >= minute;
   };
   const promoDiscount = (() => {
-    if (!isAfterOrAtWibTime(19, 45)) return 0;
+    if (!isAfterOrAtWibTime(16, 0)) return 0;
     if (method === 'gamepass' && activeRobux >= 2000) return 20000;
     if (method !== 'group') return 0;
-    if (activeRobux >= 2000) return 20000;
-    if (activeRobux >= 4000) return 35000;
-    if (activeRobux >= 5000) return 45000;
+    if (activeRobux >= 2000 && activeRobux < 4000) return 20000;
+    if (activeRobux >= 4000 && activeRobux < 5000) return 35000;
+    if (activeRobux >= 5000 && activeRobux < 10000) return 45000;
     if (activeRobux >= 10000) return 80000;
     return 0;
   })();
